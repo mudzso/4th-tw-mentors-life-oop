9882ecc07a99da50f9b799afb6e81667cdf66b9f
Telakos <tel.akos.2@gmail.com>
2017-01-11 15:13:14 +0100

    kitchen specific methods be done started lounge specific methods

--- cc_building.py
+++ cc_building.py
@@ -1,4 +1,5 @@
 from codecool_class import CodeCoolClass
+from student import Student
 import random
 
 
@@ -25,5 +26,40 @@ class Building():
                 gender_str = cc_class.get_student_gender_for_print(student, 2)
                 energy = student.energy
             print("{} drank some coffe and {} energy increased by 10".format(student.name, gender_str))
+        else:
+            pass
+
+    def drink_tea_and_chat(self, cc_class):
+        if self.is_kitchen:
+            people_in_kitchen = []
+            people = []
+            people.append(cc_class.students)
+            people.append(cc_class.mentor)
+            number_of_people = random.randint(2, 6)
+            for i in range(number_of_people):
+                people_in_kitchen.append(random.choice(people))
+            for person in people_in_kitchen:
+                if isinstance(person, Student):
+                    person.energy += 5
+                print(person, " and", end=' ')
+            print("had a cup of tea and chat together")
+        else:
+            pass
+
+    # Lounge specific methods
+    def play_a_game_of_darts(self, cc_class):
+        if self.is_lounge:
+            people_in_lounge = []
+            people = []
+            people.append(cc_class.students)
+            people.append(cc_class.mentor)
+            number_of_people = random.randint(2, 6)
+            for i in range(number_of_people):
+                people_in_lounge.append(random.choice(people))
+            for person in people_in_lounge:
+                if isinstance(person, Student):
+                    person.energy += 10
+                print(person, " and", end=' ')
+            print("played a good game of darts")
         else:
             pass
