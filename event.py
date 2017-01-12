200ed5cc06ceb6c82e041d6fbea74112aed6fd44
Nagy Márton <mudzso@gmail.com>
2017-01-12 10:51:47 +0100

    add some functions

--- event.py
+++ event.py
@@ -24,6 +24,10 @@ class Event():
     def get_drunk(self):
         if self.drink_alcohol:
             alcohols = ["Pálinka", "Sör", "Whisky", "Szaké"]
+            palinka_counter = 0
+            sor_counter = 0
+            whisky_counter = 0
+            szake_counter = 0
             drunk = False
             overall_knowledge = self.CClass.get_student_all_knowledge()
             print("Baszunk beljebb")
@@ -33,18 +37,29 @@ class Event():
                 alcohol = random.choice(alcohols)
 
                 if overall_energy_level < 0:
+                    students = len(self.CClass.students)
                     print("Megérkeztünk gyöngyöspatára!")
+                    print("{} feles pálinka,{} liter sör, {} whisky kóla,{} feles szaké fogyot el".format(palinka_counter * students,
+                                                                                                          sor_counter * students, whisky_counter * students, szake_counter * students))
                     drunk = True
 
                 elif alcohol == "Pálinka":
                     print("Kanpai! donburibachi ha-dodorinku")
-                    overall_energy_level -= 80
+                    overall_energy_level -= 300
+                    palinka_counter += 1
                 elif alcohol == "Sör":
                     print("Kanpai! nomimono bakushu ")
-                    overall_energy_level -= 30
+                    overall_energy_level -= 100
+                    sor_counter += 1
                 elif alcohol == "Whisky":
                     print("Kanpai pota-ju ba-bon")
-                    overall_energy_level -= 50
+                    overall_energy_level -= 200
+                    whisky_counter += 1
                 elif alcohol == "Szaké":
                     print("Kanpai! utsuwa hyakuyakunochou")
-                    overall_energy_level -= 60
+                    overall_energy_level -= 180
+                    szake_counter += 1
+
+miskolc = CodecoolClass.generate_local()
+beba = Event.organize_event(miskolc)
+beba.get_drunk()
